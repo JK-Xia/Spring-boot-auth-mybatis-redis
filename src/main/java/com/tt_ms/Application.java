c8dbcd9daf81466c070b21cc97bf27ba5150e4b3
wankunxin <1051663679@qq.com>
2018-04-14 13:54:49 +0800

    123

--- src/main/java/com/tt_ms/Application.java
+++ src/main/java/com/tt_ms/Application.java
@@ -3,8 +3,9 @@ package com.tt_ms;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude= {DataSourceAutoConfiguration.class})
 @MapperScan("com.tt_ms.dao")
 public class Application {
 
