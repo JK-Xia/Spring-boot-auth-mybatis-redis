ac5e4f97fe7ce3a348748c41d3f91107987e76b1
wankunxin <1051663679@qq.com>
2018-04-14 16:55:15 +0800

    added mappers.xml

---
+++ src/main/java/com/tt_ms/domain/TT1_user.java
@@ -0,0 +1,73 @@
+package com.tt_ms.domain;
+
+public class TT1_user {
+    private String userId;
+
+    private String password;
+
+    private String name;
+
+    private String sex;
+
+    private String position;
+
+    private String companyId;
+
+    private Integer status;
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId == null ? null : userId.trim();
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password == null ? null : password.trim();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name == null ? null : name.trim();
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex == null ? null : sex.trim();
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position == null ? null : position.trim();
+    }
+
+    public String getCompanyId() {
+        return companyId;
+    }
+
+    public void setCompanyId(String companyId) {
+        this.companyId = companyId == null ? null : companyId.trim();
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+}
